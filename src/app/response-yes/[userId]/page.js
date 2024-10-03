f01e3a71456a8aee73afbaf6b1222650244107fd
Casey <dinnerdoggy@gmail.com>
2024-10-02 20:28:26 -0500

    completed vid 4

--- src/app/response-yes/[userId]/page.js
+++ src/app/response-yes/[userId]/page.js
@@ -3,10 +3,8 @@ import FactCard from '@/components/Card';
 import PropTypes from 'prop-types';
 
 export default async function ResponseYesPage({ params }) {
-  const response = await fetch(`https://first-react-app-39662-default-rtdb.firebaseio.com/responseYes.json?orderBy="userId"&equalTo="${params.userId}"`);
+  const response = await fetch(`https://first-react-app-39662-default-rtdb.firebaseio.com/responseYes.json?orderBy="userId"&equalTo="${params.userId}"`, { cache: 'no-store' });
   const facts = await response.json();
-
-  console.log(Object.values(facts));
 
   return (
     <div>
