792613551d27d768a80888188ce053606df08929
Casey <dinnerdoggy@gmail.com>
2024-10-02 19:14:02 -0500

    completed tasks for vid 3

--- src/app/response-no/[userId]/page.js
+++ src/app/response-no/[userId]/page.js
@@ -1,8 +1,20 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import FactCard from '@/components/Card';
+import PropTypes from 'prop-types';
 
-export default function ResponseNoPage({ params }) {
-  return <div>{params.userId}</div>;
+export default async function ResponseNoPage({ params }) {
+  const response = await fetch(`https://first-react-app-39662-default-rtdb.firebaseio.com/responseNo.json?orderBy="userId"&equalTo="${params.userId}"`);
+  const facts = await response.json();
+
+  console.log(Object.values(facts));
+
+  return (
+    <div>
+      {Object.values(facts).map((fact) => (
+        <FactCard fact={fact.text} />
+      ))}
+    </div>
+  );
 }
 
 ResponseNoPage.propTypes = PropTypes.string;
--- src/app/response-yes/[userId]/page.js
+++ src/app/response-yes/[userId]/page.js
@@ -1,18 +1,20 @@
-// import React from 'react'
-// import FactCard from '@/components/Card';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import FactCard from '@/components/Card';
+import PropTypes from 'prop-types';
 
-// export default async function ResponseYesPage({ params }) {
-//   const response = await fetch(`https://first-react-app-39662-default-rtdb.firebaseio.com/responseYes.json?orderBy="userId"&equalTo="${params.userId}"`);
-//   const facts = await response.json();
+export default async function ResponseYesPage({ params }) {
+  const response = await fetch(`https://first-react-app-39662-default-rtdb.firebaseio.com/responseYes.json?orderBy="userId"&equalTo="${params.userId}"`);
+  const facts = await response.json();
 
-//   console.log(Object.values(facts))
+  console.log(Object.values(facts));
 
-//   return (
-//     <div>
-//       {Object.values(facts).map((fact) => <FactCard />)}
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      {Object.values(facts).map((fact) => (
+        <FactCard fact={fact.text} />
+      ))}
+    </div>
+  );
+}
 
-// ResponseYesPage.propTypes = PropTypes.string
+ResponseYesPage.propTypes = PropTypes.string;
